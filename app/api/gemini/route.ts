4f30cd539190e451c93847aa3bd947892b15e3bb
lemajr <erickbale360@gmail.com>
2024-12-19 00:44:20 +0300

    Chat bot assistant added successfully and tested for deployment

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -1,57 +1,6 @@
-// import { NextResponse } from "next/server";
-// import { GoogleGenerativeAI } from "@google/generative-ai";
-
-// const API_KEY = process.env.GOOGLE_GENERATIVE_AI_API_KEY;
-
-// if (!API_KEY) {
-//   throw new Error("Missing GOOGLE_GENERATIVE_AI_API_KEY in environment variables.");
-// }
-
-// const genAI = new GoogleGenerativeAI(API_KEY);
-// const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-// export async function POST(req: Request) {
-//   try {
-//     const { prompt } = await req.json();
-
-//     if (!prompt || typeof prompt !== "string" || !prompt.trim()) {
-//       return NextResponse.json({ error: "Invalid or empty prompt." }, { status: 400 });
-//     }
-
-//     const result = await model.generateContentStream(prompt);
-
-
-//     const encoder = new TextEncoder();
-//     const readableStream = new ReadableStream({
-//       async start(controller) {
-//         try {
-//           for await (const chunk of result.stream) {
-//             controller.enqueue(encoder.encode(chunk.text()));
-//           }
-//         } catch (streamError) {
-//           console.error("Stream error:", streamError);
-//           controller.close();
-//         }
-//         controller.close();
-//       },
-//     });
-
-//     return new Response(readableStream, {
-//       headers: { "Content-Type": "text/plain" },
-//     });
-//   } catch (error: any) {
-//     console.error("Error generating content:", error);
-
-//     const errorMessage =
-//       error?.response?.data?.message || "An error occurred while generating content.";
-//     const status = error?.response?.status || 500;
-
-//     return NextResponse.json({ error: errorMessage }, { status });
-//   }
-// 
-
 import { streamText, Message } from "ai"
 import { createGoogleGenerativeAI } from "@ai-sdk/google"
+import { initialMessage } from "@/lib/ai/data";
 
 const google = createGoogleGenerativeAI({
   apiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY || "",
@@ -65,8 +14,7 @@ const buildGoogleGenAIPrompt = (messages: Message[]): Message[] => [
   {
     id: generateId(),
     role: "user",
-    content: "You are an AI assistant. You will be given a task. You must generate a detailed and long answer.",
-    // content: initialMessage.content,
+    content: initialMessage.content,
   },
   ...messages.map((message) => ({
     id: generateId(),
---
+++ lib/ai/data.ts
@@ -0,0 +1,8 @@
+export const initialMessage = {
+    role: "system",
+    content:
+      "Welcome to UniDrive, your dedicated university storage assistant. UniDrive is a secure platform designed to support students and lecturers in sharing and accessing essential educational resources, such as lecture notes, past exam papers, tests, and assignments. Our goal is to streamline your academic experience by providing easy and efficient access to these materials. \n\n" +
+      "Please be aware that I am specifically equipped to assist with queries related to UniDrive. Should your inquiry fall outside of this scope, I must respectfully inform you that I will not be able to provide an answer. In such cases, kindly note the following response: \n\n" +
+      "'I regret that I can only provide assistance with UniDrive-related matters. For inquiries beyond this scope, I am unable to offer further information.'"
+  };
+  

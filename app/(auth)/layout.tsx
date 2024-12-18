8cedd9d1f2d0c627cca6a07e52a232ec605a9cb8
lemajr <erickbale360@gmail.com>
2024-12-19 00:59:38 +0300

    deployment phase 1

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (     
 
-      <section className="flex min-h-screen flex-col items-center bg-white p-4 py-20 justify-center lg:p-10 lg:py-0">
-        <div className="mb-16 lg:justify-center">
+      <section className="flex min-h-svh flex-col items-center bg-white p-10 py-20 justify-center lg:p-10 lg:py-0">
+        <div className="lg:mb-16 mb-8 lg:justify-center">
           <Image
             src="/logo.png"
             alt="logo"
-            width={224}
-            height={82}
+            width={200}
+            height={200}
             className="h-12 w-[224px]"
           />
         </div>
